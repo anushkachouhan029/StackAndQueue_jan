9c0ebbcb96e56256f02a84048490489509463930
anushkachouhan029 <anushkachouhan029>
2023-01-20 21:18:01 +0530

    added uc1 create stack

---
+++ UC1_CreateStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack and Queue program \n---------------------------------------");
+            Stack stack = new Stack();
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+
+            stack.Push(node1);
+            stack.Push(node2);
+            stack.Push(node3);
+            stack.DisplayStack();
+
+        }
+    }
+}
---
+++ UC1_CreateStack/Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        LinkedList linkedlist;
+        public Stack()
+        {
+            linkedlist = new LinkedList();
+        }
+        public void Push(Node node)
+        {
+            linkedlist.AddNode(node);
+        }
+        public void DisplayStack()
+        {
+            linkedlist.StackDisplay();
+        }
+    }
